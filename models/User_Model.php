7babb2bf68d81b83be98714f8092c5f7782fcc95
lowercase-tousif <toisiftasrik@gmail.com>
2025-08-07 01:44:11 +0600

    User model added: Login, Register Done

---
+++ models/User_Model.php
@@ -0,0 +1,49 @@
+<?php
+
+require_once __DIR__ . '../../config/Database.php';
+
+class User
+{
+    private $db;
+
+    public function __construct()
+    {
+        $this->db = (new Database())->getConnection();
+    }
+
+
+    public function register($username, $password, $role = 'guest')
+    {
+        $hashed_password = password_hash($password, PASSWORD_BCRYPT);
+        $sql = "INSERT INTO user_info(user_name, user_pass, user_role) VALUES(:username, :password, :role)";
+
+        $stmt = oci_parse($this->db, $sql);
+
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_bind_by_name($stmt, ':password', $hashed_password);
+        oci_bind_by_name($stmt, ':role', $role);
+
+        return oci_execute($stmt);
+    }
+
+
+    public function login($username, $password)
+    {
+        $sql = "SELECT *
+                FROM user_info
+                WHERE username = :username";
+
+        $stmt = oci_parse($this->db, $sql);
+
+        oci_bind_by_name($stmt, ':username', $username);
+        oci_execute($stmt);
+
+        $existing_user = oci_fetch_assoc($stmt);
+
+        if ($existing_user && password_verify($password, $existing_user['USER_PASS'])) {
+            return $existing_user;
+        }
+
+        return false;
+    }
+}
