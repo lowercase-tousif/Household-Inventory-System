5807e93603a71145728779ee5b7d2538a7ee940d
lowercase-tousif <toisiftasrik@gmail.com>
2025-08-08 19:38:53 +0600

    Register, Login view completed

---
+++ views/Auth/login.php
@@ -0,0 +1,6 @@
+<form method="POST">
+    <h2>Login</h2>
+    Username: <input type="text" name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    <input type="submit" value="Login">
+</form>
---
+++ views/Auth/register.php
@@ -0,0 +1,13 @@
+<form method="POST">
+    <h2>Register</h2>
+    Username: <input type="text" name="username" required><br>
+    Password: <input type="password" name="password" required><br>
+    Role: 
+    <select name="role">
+        <option value="admin">Admin</option>
+        <option value="family">Family Member</option>
+        <option value="guest">Guest</option>
+    </select><br>
+    <input type="submit" value="Register">
+</form>
+    
