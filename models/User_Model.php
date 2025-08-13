492f57c5b0503f7e6ea9bd13e2794e7ae613ac98
lowercase-tousif <toisiftasrik@gmail.com>
2025-08-13 13:41:01 +0600

    Usermodel some changes made

--- models/User_Model.php
+++ models/User_Model.php
@@ -1,6 +1,5 @@
 <?php
-
-require_once __DIR__ . '../../config/Database.php';
+require_once __DIR__ . "/../config/Database.php";
 
 class User
 {
@@ -11,30 +10,23 @@ class User
         $this->db = (new Database())->getConnection();
     }
 
-
     public function register($username, $password, $role = 'guest')
     {
         $hashed_password = password_hash($password, PASSWORD_BCRYPT);
         $sql = "INSERT INTO user_info(user_name, user_pass, user_role) VALUES(:username, :password, :role)";
-
         $stmt = oci_parse($this->db, $sql);
 
         oci_bind_by_name($stmt, ':username', $username);
         oci_bind_by_name($stmt, ':password', $hashed_password);
         oci_bind_by_name($stmt, ':role', $role);
 
-        return oci_execute($stmt);
+        return oci_execute($stmt, OCI_COMMIT_ON_SUCCESS);
     }
-
 
     public function login($username, $password)
     {
-        $sql = "SELECT *
-                FROM user_info
-                WHERE username = :username";
-
+        $sql = "SELECT * FROM user_info WHERE user_name = :username";
         $stmt = oci_parse($this->db, $sql);
-
         oci_bind_by_name($stmt, ':username', $username);
         oci_execute($stmt);
 
