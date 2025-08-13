b356ef02f82d10dabb33ce9ad0745d3af94eefe8
lowercase-tousif <toisiftasrik@gmail.com>
2025-08-13 11:21:31 +0600

    Login page  have a fresh look

--- views/Auth/login.php
+++ views/Auth/login.php
@@ -1,6 +1,23 @@
-<form method="POST">
-    <h2>Login</h2>
-    Username: <input type="text" name="username" required><br>
-    Password: <input type="password" name="password" required><br>
-    <input type="submit" value="Login">
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Login Page</title>
+<link rel="stylesheet" href="../../public/css/login.css">
+</head>
+<body>
+
+<div class="image-side"></div>
+
+<div class="form-side">
+    <form method="POST">
+        <h2>Welcome to the login page</h2>
+        <input type="text" name="username" placeholder="Enter Username" required>
+        <input type="password" name="password" placeholder="Enter Password" required>
+        <input type="submit" value="Login">
+    </form>
+</div>
+
+</body>
+</html>
