238e7cf70165ad95d5eb91564dc9aa33b0c1b54c
lowercase-tousif <toisiftasrik@gmail.com>
2025-08-14 10:46:05 +0600

    Css can be seen and navigation fixed

--- views/Auth/login.php
+++ views/Auth/login.php
@@ -1,23 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<title>Login</title>
-<link rel="stylesheet" href="../../public/css/register.css">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login</title>
+  <link rel="stylesheet" href="/household-inventory-system/views/css/login.css">
 </head>
 <body>
-<form action="/household-inventory-system/index.php?action=login" method="POST">
-    <h2>Login</h2>
-    <?php if(!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
-    <div class="input-group">
+
+  <div class="image-side"></div>
+
+  <div class="form-side">
+    <form action="/household-inventory-system/index.php?action=login" method="POST">
+      <h2>Login</h2>
+      <?php if (!empty($error)) echo "<p style='color:red;'>$error</p>"; ?>
+      <div class="input-group">
         <input type="text" name="username" placeholder="Enter Username" required>
-    </div>
-    <div class="input-group">
+      </div>
+      <div class="input-group">
         <input type="password" name="password" placeholder="Enter Password" required>
-    </div>
-    <p>New to the application ? <a href="register.php">register here</a></p>
-    <button type="submit">Login</button>
-</form>
+      </div>
+      <p>New to the application ? <a href="../Auth/register.php">register here</a></p>
+      <input type="submit" value="Login">
+    </form>
+  </div>
+
 </body>
 </html>
